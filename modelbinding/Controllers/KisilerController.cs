20143523e28a5b80ad9dd9ce0adda35d294316e5
haybek <haybekbatuhan@gmail.com>
2023-08-07 11:12:20 +0300

    Add project files.

---
+++ modelbinding/Controllers/KisilerController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using modelbinding.Models;
+
+namespace modelbinding.Controllers
+{
+    public class KisilerController : Controller
+    {
+        static List<Kisi> kisiler = new List<Kisi>() {  // burada normalde static yerine veritabanindan cekiyoruz
+        
+            new Kisi() { Ad = "Ali"},
+            new Kisi() { Ad = "Ece"},
+            new Kisi() { Ad = "Can"},
+            new Kisi() { Ad = "Ela"},
+        };
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Yeni()
+        {
+            return View();
+        }
+    }
+}
---
+++ modelbinding/Models/Kisi.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace modelbinding.Models
+{
+    public class Kisi
+    {
+        [Required(ErrorMessage = "Ad alani zorunludur")] // ad alaninda Ad yerine {0} da kullanabiliriz
+        [MaxLength(20, ErrorMessage = "Ad alani en fazla 20 karakter olabilir")]
+        // bu ikisine data annotation deniyor
+        public string Ad { get; set; } = null!;
+    }
+}
