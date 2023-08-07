2557d4ae2e7afcffea12c6433094fc28ba5c0f7d
haybek <haybekbatuhan@gmail.com>
2023-08-07 12:38:01 +0300

    7 agustos mvc uygulama teori dersi

--- modelbinding/Controllers/KisilerController.cs
+++ modelbinding/Controllers/KisilerController.cs
@@ -3,6 +3,7 @@ using modelbinding.Models;
 
 namespace modelbinding.Controllers
 {
+    //[Route("Kisiler")] dersek localhost:5000/Kisiler/Abidik yazinca bu controller calisir
     public class KisilerController : Controller
     {
         static List<Kisi> kisiler = new List<Kisi>() {  // burada normalde static yerine veritabanindan cekiyoruz
@@ -13,13 +14,27 @@ namespace modelbinding.Controllers
             new Kisi() { Ad = "Ela"},
         };
 
+        //[Route("Abidik")] dersek localhost:5000/Abidik yazinca bu action calisir
+        // Get: Kisiler
         public IActionResult Index()
+        {
+            return View(kisiler);
+        }
+        // Get: Kisiler/Yeni
+        public IActionResult Yeni()
         {
             return View();
         }
-
-        public IActionResult Yeni()
+        // Post: Kisiler/Yeni
+        [HttpPost] // post islemlerini buna yonlendiriyor
+        public IActionResult Yeni(Kisi kisi)
         {
+            // burada ne yapacagiz tekrardan ayni view verisini dondurmemeliyiz
+            if (ModelState.IsValid)
+            {
+                kisiler.Add(kisi);
+                return RedirectToAction("Index");
+            }
             return View();
         }
     }
